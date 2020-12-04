f2c524deb60fbd04214a9b6d38b7919162cca9cf
Nilofer Rajpurkar <nirajpur@microsoft.com>
2020-12-03 22:19:44 -0800

    Redo 2 with regex

--- python/02/02.py
+++ python/02/02.py
@@ -1,5 +1,12 @@
+import re
+
 password_file = open('python/02/passwords.txt', 'r')
 passwords = password_file.readlines()
+password_file.close()
+
+password_file = open('python/02/passwords.txt', 'r')
+passwords_string = password_file.read()
+password_file.close()
 valid_password = 0
 
 # Part 1
@@ -16,6 +23,25 @@ for j in range(len(passwords)):
             char_count = char_count + 1
     if (char_count >= char_min) and (char_count <= char_max):
         valid_password = valid_password + 1
+
+print(valid_password)
+
+valid_password = 0
+# Part 1 using regex
+pass_regex = re.compile(r'(\d+)-(\d+)\s(\w):\s(\w+)')
+match_pass = pass_regex.findall(passwords_string)
+
+for j in range(len(match_pass)):
+    char_min = int(match_pass[j][0])
+    char_max = int(match_pass[j][1])
+    char_required = match_pass[j][2]
+    current_password = match_pass[j][3]
+    char_count = 0
+    for i in range(len(current_password)):
+        if current_password[i] == char_required:
+            char_count += 1
+    if (char_count >= char_min) and (char_count <= char_max):
+        valid_password += 1
 
 print(valid_password)
 
